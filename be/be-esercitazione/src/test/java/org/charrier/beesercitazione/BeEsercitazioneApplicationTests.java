9cb37e900f696e5547d804a89b243d07171f0286
MatteoCharrierITS <matteo.charrier@edu-its.it>
2026-06-09 14:52:51 +0200

    Postgres DB + Flyway Migration v1

---
+++ be/be-esercitazione/src/test/java/org/charrier/beesercitazione/BeEsercitazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.charrier.beesercitazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEsercitazioneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
